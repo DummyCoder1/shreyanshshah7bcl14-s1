995256dc154cfc3b4b55c5cf7048c97fc25e20d0
DummyCoder1 <100186733+DummyCoder1@users.noreply.github.com>
2022-04-09 08:27:08 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,15 @@
+var colors = ["white","orange","red","green"];
+
+function setup() {
+  createCanvas(innerWidth, innerHeight);
+  background("white");
+}
+
+function draw() {
+  var y =50;
+  for (i = 0; i < colors.length; i++){
+     currentColor = new Ball(width/2,y,50,colors[i]);
+     y = y + 50;
+     currentColor.appear();
+ }
+}
